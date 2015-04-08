c33d3fbe8d99aa03cf871b379e974a8287c4fbc1
fringe4life <coinnich09@gmail.com>
2015-04-08 20:43:35 +1200

    small fixes

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 
 set_include_path('./php-includes' . PATH_SEPARATOR . './functions');
 
-
+session_start();
+require_once 'functions/csrf_token_functions.php';
 // Includes
 require_once 'connect.inc.php';
 require_once 'head.inc.html';
 require_once 'header.inc.html';
 
-session_start();
-require_once 'functions/csrf_token_functions.php';
+
 
 require_once 'tab-1.php';
 require_once 'footer.inc.html';
